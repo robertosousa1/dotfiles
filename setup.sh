dd758bbfd3b69adc9bc08abb82f4cfd1d67001f9
Roberto Alves <roberto_junior_sousa@hotmail.com>
2020-12-05 14:30:20 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -159,6 +159,7 @@ code --install-extension tomoki1207.pdf
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension pkief.material-icon-theme
 code --install-extension snyk-security.vscode-vuln-cost
+code --install-extension natqe.reload
 
 echo 'installing fira code'
 brew tap homebrew/cask-fonts
